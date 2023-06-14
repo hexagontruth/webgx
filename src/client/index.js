c3fb5ab17f4c2f38ce26057a9eb2a994cb8c8c25
graham <graham@hexagon.space>
2023-06-14 13:39:46 +0700

    remove unused packages and update readme

--- src/client/index.js
+++ src/client/index.js
@@ -1,8 +1,5 @@
 import * as dat from 'dat.gui';
 
-import '@fontsource/montserrat';
-import '@fontsource/roboto';
-import '@fontsource/roboto-mono';
 import './styles/style.scss';
 
 import App from './classes/app';
