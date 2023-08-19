98c89f283a4ddb999c03a856c52a8fd58896abad
graham <graham@hexagon.space>
2023-08-19 17:50:57 +0800

    Make data array optional in DataBuffer

--- src/client/classes/data-buffer.js
+++ src/client/classes/data-buffer.js
@@ -19,7 +19,7 @@ export default class DataBuffer {
     Uint32Array: 'uint32',
   };
 
-  static parseTypeData(str) {
+  static parseType(str) {
     // Assuming Float16Array will be added at some point?
     // Length property isn't doing anything at the moment
     const match = str.match(/^(u|s)?([a-z]+)(\d{1,2})(x(\d))?$/);
@@ -28,45 +28,49 @@ export default class DataBuffer {
     }
     const signed = match[1] != 'u';
     const type = match[2];
-    const size = Number(match[3]);
+    const size = Number(match[3]) / 8;
     const length = Number(match[5]) || 1;
     let constructorName = signed == 'u' ? 'u' : '';
     constructorName += type == 'int' ? 'int' : 'float';
     constructorName = constructorName[0].toUpperCase() + constructorName.slice(1);
-    constructorName += size;
+    constructorName += size * 8;
     constructorName += 'Array';
     const constructor = window[constructorName];
     return { signed, type, size, length, constructor };
   }
 
-  constructor(device, data, flags, type) {
+  constructor(device, dataArg, flags, type) {
     this.device = device;
     this.flags = flags ?? this.constructor.defaultFlags;
     this.flagSet = Object.entries(GPUBufferUsage).filter((e) => e[1] & this.flags).map((e) => e[0]);
-    data && this.setData(data, type);
-  }
 
-  setData(data, type) {
-    if (ArrayBuffer.isView(data)) {
-      this.data = data;
-      this.type = type || this.constructor.defaultTypeMap[data.constructor.name];
-      this.typeData = DataBuffer.parseTypeData(this.type);
+    if (ArrayBuffer.isView(dataArg)) {
+      this.data = dataArg;
+      this.type = type || this.constructor.defaultTypeMap[this.data.constructor.name];
+      this.typeData = DataBuffer.parseType(this.type);
+      this.length = this.data.length;
     }
     else {
       this.type = type || this.constructor.defaultType;
-      this.typeData = DataBuffer.parseTypeData(this.type);
-      this.data = new this.typeData.constructor(data);
+      this.typeData = DataBuffer.parseType(this.type);
+      if (Number.isFinite(dataArg)) {
+        this.length = dataArg;
+      }
+      else {
+        this.data = new this.typeData.constructor(dataArg);
+        this.length = this.data.length;
+      }
     }
-    this.length = this.data.length;
-    this.byteLength = this.data.byteLength;
-    this.typeSize = this.byteLength / this.length;
-
+    // This is not ideal terminology
+    this.typeSize = this.typeData.size * this.typeData.length;
+    this.byteLength = this.typeSize * this.length;
     this.buffer = this.device.createBuffer({
       size: this.byteLength,
       usage: this.flags,
     });
 
-    this.hasFlag(GPUBufferUsage.COPY_DST) && this.write();
+    this.hasFlag(GPUBufferUsage.COPY_DST) &&  this.write();
+
   }
 
   hasFlag(flag) {
